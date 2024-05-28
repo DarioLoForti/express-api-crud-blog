4334171a937cadef0ec426e93205aca9bd18b804
Dario Lo Forti <lofortidario@gmail.com>
2024-05-28 16:59:01 +0200

    chore: Add morgan logging middleware for request logging

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
+const morgan = require('morgan');
 const postsRouter = require('./routers/posts');
+
 const errorsFormatter = require('./middlewares/errorsFormatter');
 const routersNotFound = require('./middlewares/routersNotFound');
+
+
 app.use(express.static('public/images'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.send('<h1>Benvenuto nel mio blog!</h1><h1>I miei <a href="http://localhost:3000/posts">Post</a></h1><style>body{background-color: black; color: white;} a{text-decoration: none; color: white;}</style>');
